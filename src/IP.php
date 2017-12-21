3456550e245a49f400bfb4d0c613c8c5c90a0779
jormin <jorminxee@gmail.com>
2017-12-21 11:25:56 +0800

    完成代码

--- src/IP.php
+++ src/IP.php
@@ -2,32 +2,37 @@
 
 namespace Jormin\IP;
 
+/**
+ * IP地址解析,使用 https://www.ipip.net/ 的服务
+ *
+ * Class IP
+ * @package Jormin\IP
+ */
 class IP
 {
 
-    private static $ip     = NULL;
-    private static $fp     = NULL;
+    private static $ip = NULL;
+    private static $fp = NULL;
     private static $offset = NULL;
-    private static $index  = NULL;
-
+    private static $index = NULL;
     private static $cached = array();
 
     /**
      * 解析IP地址
      *
      * @param $ip
+     * @param bool $implode 是否组合字符串
+     * @param string $glue 数组元素之间放置的内容
      * @return mixed|string
      */
-    public static function ip2addr($ip)
+    public static function ip2addr($ip, $implode = false, $glue = ' ')
     {
         if (empty($ip) === TRUE)
         {
             return 'N/A';
         }
-
-        $nip   = gethostbyname($ip);
+        $nip = gethostbyname($ip);
         $ipdot = explode('.', $nip);
-
         if ($ipdot[0] < 0 || $ipdot[0] > 255 || count($ipdot) !== 4)
         {
             return 'N/A';
@@ -37,17 +42,13 @@ class IP
         {
             return self::$cached[$nip];
         }
-
         if (self::$fp === NULL)
         {
             self::init();
         }
-
         $nip2 = pack('N', ip2long($nip));
-
         $tmp_offset = (int)$ipdot[0] * 4;
-        $start      = unpack('Vlen', self::$index[$tmp_offset] . self::$index[$tmp_offset + 1] . self::$index[$tmp_offset + 2] . self::$index[$tmp_offset + 3]);
-
+        $start = unpack('Vlen', self::$index[$tmp_offset] . self::$index[$tmp_offset + 1] . self::$index[$tmp_offset + 2] . self::$index[$tmp_offset + 3]);
         $index_offset = $index_length = NULL;
         $max_comp_len = self::$offset['len'] - 1024 - 4;
         for ($start = $start['len'] * 8 + 1024; $start < $max_comp_len; $start += 8)
@@ -56,21 +57,17 @@ class IP
             {
                 $index_offset = unpack('Vlen', self::$index{$start + 4} . self::$index{$start + 5} . self::$index{$start + 6} . "\x0");
                 $index_length = unpack('Clen', self::$index{$start + 7});
-
                 break;
             }
         }
-
         if ($index_offset === NULL)
         {
             return 'N/A';
         }
-
         fseek(self::$fp, self::$offset['len'] + $index_offset['len'] - 1024);
-
         self::$cached[$nip] = explode("\t", fread(self::$fp, $index_length['len']));
-
-        return self::$cached[$nip];
+        $ipArr = self::$cached[$nip];
+        return $implode ? implode($glue, $ipArr) : $ipArr;
     }
 
     /**
@@ -81,19 +78,16 @@ class IP
         if (self::$fp === NULL)
         {
             self::$ip = new self();
-
             self::$fp = fopen(__DIR__ . '/../dat/17monipdb.dat', 'rb');
             if (self::$fp === FALSE)
             {
                 throw new \Exception('Invalid 17monipdb.dat file!');
             }
-
             self::$offset = unpack('Nlen', fread(self::$fp, 4));
             if (self::$offset['len'] < 4)
             {
                 throw new \Exception('Invalid 17monipdb.dat file!');
             }
-
             self::$index = fread(self::$fp, self::$offset['len'] - 4);
         }
     }
